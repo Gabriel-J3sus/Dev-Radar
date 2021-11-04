22f73c79f8d1e86e71f22495bbf1229d6516117c
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-11-04 10:42:03 -0300

    feat(backend): type utilities by PAULITO

--- packages/backend/src/entities/User.ts
+++ packages/backend/src/entities/User.ts
@@ -7,4 +7,14 @@ export type UserEntityWithoutPassword = Omit<User, 'password'>
 export type UserCreateInputType = Prisma.UserCreateInput
 export type UserFindUniqueArgsType = Prisma.UserFindUniqueArgs
 export type UserUpdateArgsType = Prisma.UserUpdateArgs
-export type UserUpdateInputType = Prisma.UserUpdateInput
+export type UserUpdateInputType = Prisma.UserUpdateInput & Required<Pick<Prisma.UserUpdateInput,'id'>>
+
+type SeparateRequired<T,R extends keyof T,O extends keyof T = Exclude<keyof T, R>> = Partial<Pick<T,O>>  & Required<Pick<T,R>>
+
+const a:SeparateRequired<Prisma.UserUpdateInput,'id'>  = {
+
+}
+
+type Keys<T> = keyof T
+
+const arrayFoda: Keys<Prisma.UserUpdateInput>[ ] = [''] 
